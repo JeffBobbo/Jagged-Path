fd514579b84abe7373b74053b3b54faee63ed02f
JeffBobbo <james.delph@hotmail.co.uk>
2014-12-02 01:09:11 +0000

    new spawner class for spawning stuff

---
+++ src/spawner.js
@@ -0,0 +1,139 @@
+/*
+  Jagged Path, javascript browser game
+  Copyright (C) 2014 Jeff Bobbo
+  All rights reserved
+*/
+
+JP.SpawnBase = function(progeny, x, y)
+{
+  this.id = JP.SpawnBase.ID++;
+  this.num = 1;
+  this.interval = 1000;
+  this.lastSpawn = 0;
+  this.limited = -1;
+
+  this.children = []; // id's of children, no direct access
+
+  // spawner center
+  this.relx = x || -1;
+  this.rely = y || -1;
+
+  // spawner area
+  this.radius = 3;
+
+  // what we can actually spawn
+  this.progeny = progeny || null;
+
+  // special spawning rules
+  this.rules = null;
+};
+JP.SpawnBase.ID = 0;
+
+
+JP.SpawnBase.prototype.ChildCount = function(valid)
+{
+  var ret = 0;
+  for (var i = this.children.length - 1; i >= 0; i--)
+  {
+    var child = JP.Entity.FindByID(this.children[i]);
+    if (child.seppuku === false) // only count those who aren't marked seppuku
+      ret++;
+  }
+  return ret;
+};
+
+JP.SpawnBase.prototype.Idle = function()
+{
+  for (var i = this.children.length - 1; i >= 0; i--)
+  {
+    var child = JP.Entity.FindByID(this.children[i]);
+    child.Idle();
+    child.Move();
+  };
+
+  this.Spawn();
+};
+
+JP.SpawnBase.prototype.CanSpawn = function()
+{
+  if (this.progeny === null)
+    return false;
+
+  if (this.limit === 0) // hit our limit, no more spawns
+    return false;
+
+  var count = this.ChildCount();
+
+  if (count >= this.num) // none free
+    return false;
+
+  if (this.lastSpawn + this.interval < JP.getTickCount)
+    return false;
+
+  return true;
+};
+
+JP.SpawnBase.prototype.Spawn = function()
+{
+  if (this.CanSpawn() === false)
+    return;
+
+  this.lastSpawn = JP.getTickCount;
+  for (var i = this.ChildCount(); i < this.num && this.limit !== 0; ++i)
+  {
+    var dir = randRange(-Math.PI, Math.PI); // pick a random direction
+    var dist = randRange(0, this.radius);   // and a random distance
+    var x = Math.cos(dir) * dist + this.relx; // and work out placement
+    var y = Math.sin(dir) * dist + this.rely; // and work out placement
+
+    var ent = JP.Entity.Create(this.progeny, x, y, null);
+    ent.spawner = this;
+
+    this.children.push(ent.id);
+    JP.world.entities.push(ent);
+
+    if (this.limit > 0)
+      this.limit--;
+  }
+};
+
+JP.SpawnItem = function()
+{
+  JP.SpawnBase.apply(this, arguments)
+
+  this.boxQuant = 1;
+  this.boxImg = null;
+};
+JP.SpawnItem.prototype = Object.create(JP.SpawnBase.prototype);
+JP.SpawnItem.prototype.constructor = JP.SpawnBase;
+
+JP.SpawnItem.prototype.Spawn = function()
+{
+  if (this.CanSpawn() === false)
+    return;
+
+  this.lastSpawn = JP.getTickCount;
+  for (var i = this.ChildCount(); i < this.num && this.limit !== 0; ++i)
+  {
+    var dir = randRange(-Math.PI, Math.PI); // pick a random direction
+    var dist = randRange(0, this.radius);   // and a random distance
+    var x = Math.cos(dir) * dist + this.relx; // and work out placement
+    var y = Math.sin(dir) * dist + this.rely; // and work out placement
+
+    var box = new JP.Entity.ItemBox(x, y);
+    box.spawner = this;
+    if (this.progeny === "Gold")
+      box.SetGold(this.boxQuant);
+    else
+      box.SetItem(this.progeny, this.boxQuant);
+    if (this.boxImg !== null)
+      box.SetImage(this.boxImg);
+
+    this.children.push(box.id);
+    JP.world.entities.push(box);
+
+    if (this.limit > 0)
+      this.limit--;
+  }
+}
+
