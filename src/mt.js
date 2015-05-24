7c8d4fd1692c9e1dfdbbc52c6891f71eb68fa28d
JeffBobbo <james.delph@hotmail.co.uk>
2015-05-24 13:52:30 +0100

    some more changes on mt

--- src/mt.js
+++ src/mt.js
@@ -1,11 +1,10 @@
 MT = function(s)
 {
-  this.stateSize = 624;
-  this.state = new Array(MT.stateSize);
+  this.state = new Uint32Array(624);
   this.index = 0;
 
   this.seed(s);
-}
+};
 
 MT.RAND_MAX = Math.pow(2, 32);
 
@@ -26,20 +25,20 @@ MT.prototype.seed = function(s)
   }
 
   this.state[0] = seed;
-  for (var i = 1; i < this.stateSize; ++i)
+  for (var i = 1; i < 624; ++i)
     this.state[i] = 0xFFFF & (0x6C078965 * (this.state[i - 1] ^ (this.state[i - 1] >> 30)) + i);
-}
+};
 
 MT.prototype.generate = function()
 {
-  for (var i = 0; i < this.stateSize; ++i)
+  for (var i = 0; i < 624; ++i)
   {
-    var j = (this.state[i] & 0x80000000) + (this.state[(i + 1) % this.stateSize] & 0x7FFFFFFF);
-    this.state[i] = this.state[(i + 397) % this.stateSize] ^ (j >> 1);
+    var j = (this.state[i] & 0x80000000) + (this.state[(i + 1) % 624] & 0x7FFFFFFF);
+    this.state[i] = this.state[(i + 397) % 624] ^ (j >> 1);
     if (j % 2 !== 0)
       this.state[i] = this.state[i] ^ 0x9908B0DF;
   }
-}
+};
 
 MT.prototype.extract = function()
 {
@@ -54,7 +53,22 @@ MT.prototype.extract = function()
   y ^= (y >> 28);
 
   this.index++;
-  if (this.index == this.stateSize)
+  if (this.index == 624)
     this.index = 0;
   return y;
-}
+};
+
+MT.prototype.rand64 = function()
+{
+  return this.extract();
+};
+
+MT.prototype.rand = function()
+{
+  return this.extract() >>> 1;
+};
+
+MT.prototype.random = function()
+{
+  return this.extract() / MT.RAND_MAX;
+};
